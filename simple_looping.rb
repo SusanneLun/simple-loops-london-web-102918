e9b8ca0e9591a091f9a8150fa53ac02f54bbb5a7
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 12:49:44 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -13,7 +14,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  7.times do
+  7.number_of_times do
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
