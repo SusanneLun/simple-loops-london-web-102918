64958a0569d61954c9d8785d20f28e1c80339e98
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 12:48:23 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,6 +33,7 @@ def until_iterator(number_of_times)
   counter += 1 
   puts "Welcome to Flatiron School's Web Development Course!"
 end
+end
 
 def for_iterator(number_of_times)
  for counter in 1..7
