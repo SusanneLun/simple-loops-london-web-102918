e562fa494cea97f1f812ae6a30dba4bb2c49713c
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 12:50:46 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def loop_iterator(number_of_times)
 end
 
 def times_iterator(number_of_times)
-  7.number_of_times do
+  7.number_of_times 
   puts "Welcome to Flatiron School's Web Development Course!"
 end
 end
