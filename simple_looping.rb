e5d6e205d42fd26978e6da2d5a95724b58e53ca3
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-24 12:42:08 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -21,7 +21,7 @@ end
 
 def while_iterator(number_of_times)
   counter = 0 
-  while counter < 8 
+  while counter < 8 do
   puts "Welcome to Flatiron School's Web Development Course!"
   counter += 1
 end
